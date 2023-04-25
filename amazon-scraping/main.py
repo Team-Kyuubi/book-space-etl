2cb980a40d893191a7ffaed99f216d75d7b82209
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-04-25 02:41:31 -0300

    Implementação do scraping de negócios e empreendedorismo

--- amazon-scraping/main.py
+++ amazon-scraping/main.py
@@ -3,6 +3,7 @@ from urls import ciencias
 from urls import desenvolvimento_pessoal
 from urls import economia
 from urls import ficcao_cientifica
+from urls import negocios
 from urls import romances
 from urls import tecnologia
 from urls import terror
@@ -10,8 +11,14 @@ from scraping_biografias import scraping_biografias
 from scraping_padrao import scraping_padrao
 from scraping_terror import scraping_terror
 
-categorias = [biografias.categoria, ciencias.categoria, desenvolvimento_pessoal.categoria, economia.categoria, ficcao_cientifica.categoria, romances.categoria, tecnologia.categoria, terror.categoria]
-links = [biografias.url_livros, ciencias.url_livros, desenvolvimento_pessoal.url_livros, economia.url_livros, ficcao_cientifica.url_livros, romances.url_livros, tecnologia.url_livros, terror.url_livros]
+categorias = [
+    biografias.categoria, ciencias.categoria, desenvolvimento_pessoal.categoria, economia.categoria,
+    ficcao_cientifica.categoria, negocios.categoria, romances.categoria, tecnologia.categoria, terror.categoria
+]
+links = [
+    biografias.url_livros, ciencias.url_livros, desenvolvimento_pessoal.url_livros, economia.url_livros,
+    ficcao_cientifica.url_livros, negocios.url_livros, romances.url_livros, tecnologia.url_livros, terror.url_livros
+]
 
 cont = 0
 while cont < len(categorias):
---
+++ amazon-scraping/urls/negocios.py
@@ -0,0 +1,14 @@
+categoria = 'Negócios e Empreendedorismo'
+
+url_livros = [
+    'https://www.amazon.com.br/Venda-informa%C3%A7%C3%A3o-perfil-vendedor-sucesso-ebook/dp/B07Q8CRL9Z/ref=sr_1_19?rnid=5475882011&s=digital-text&sr=1-19',
+    'https://www.amazon.com.br/Modelo-Toyota-Excel%C3%AAncia-Servi%C3%A7os-Transforma%C3%A7%C3%A3o-ebook/dp/B07RCYDKLP/ref=sr_1_21?rnid=5475882011&s=digital-text&sr=1-21',
+    'https://www.amazon.com.br/Simplificando-neg%C3%B3cios-Lideran%C3%A7a-Marketing-Produtividade-ebook/dp/B0BXV7V8WP/ref=sr_1_7?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=neg%C3%B3cios&s=digital-text&sr=1-7',
+    'https://www.amazon.com.br/Mestres-escala-transformaram-pequenos-neg%C3%B3cios-ebook/dp/B0BS762KWL/ref=sr_1_8?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=negocios&s=digital-text&sr=1-8',
+    'https://www.amazon.com.br/f%C3%B3rmula-lan%C3%A7amento-estrat%C3%A9gias-secretas-line-ebook/dp/B07S6KYFQN/ref=sr_1_9?keywords=negocios&s=digital-text&sr=1-9',
+    'https://www.amazon.com.br/Contabilidade-Empresarial-Instrumento-An%C3%A1lise-Ger%C3%AAncia-ebook/dp/B07GBFNPLF/ref=sr_1_35?rnid=5475882011&s=digital-text&sr=1-35',
+    'https://www.amazon.com.br/mente-do-empreendedor-Kevin-Johnson-ebook/dp/B07NWZCMT7/ref=sr_1_2?keywords=empreendedorismo&s=digital-text&sr=1-2',
+    'https://www.amazon.com.br/Empreendedorismo-plano-neg%C3%B3cios-li%C3%A7%C3%B5es-2ed-ebook/dp/B07Z3Z52ZL/ref=sr_1_44?keywords=empreendedorismo&s=digital-text&sr=1-44',
+    'https://www.amazon.com.br/Scaling-Up-Escalando-seu-neg%C3%B3cio-ebook/dp/B08VJN4LP8/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=neg%C3%B3cios&s=digital-text&sr=1-6',
+    'https://www.amazon.com.br/Instagram-WhatsApp-Facebook-para-Neg%C3%B3cios-ebook/dp/B0924ZCLNH/ref=sr_1_19?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=neg%C3%B3cios&s=digital-text&sr=1-19'
+]
