eef0b7584225409d152abfb574265a294b8b2a35
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-04-25 02:31:34 -0300

    Implementação do scraping de economia

--- amazon-scraping/main.py
+++ amazon-scraping/main.py
@@ -1,6 +1,7 @@
 from urls import biografias
 from urls import ciencias
 from urls import desenvolvimento_pessoal
+from urls import economia
 from urls import ficcao_cientifica
 from urls import romances
 from urls import tecnologia
@@ -9,8 +10,8 @@ from scraping_biografias import scraping_biografias
 from scraping_padrao import scraping_padrao
 from scraping_terror import scraping_terror
 
-categorias = [biografias.categoria, ciencias.categoria, desenvolvimento_pessoal.categoria, ficcao_cientifica.categoria, romances.categoria, tecnologia.categoria, terror.categoria]
-links = [biografias.url_livros, ciencias.url_livros, desenvolvimento_pessoal.url_livros, ficcao_cientifica.url_livros, romances.url_livros, tecnologia.url_livros, terror.url_livros]
+categorias = [biografias.categoria, ciencias.categoria, desenvolvimento_pessoal.categoria, economia.categoria, ficcao_cientifica.categoria, romances.categoria, tecnologia.categoria, terror.categoria]
+links = [biografias.url_livros, ciencias.url_livros, desenvolvimento_pessoal.url_livros, economia.url_livros, ficcao_cientifica.url_livros, romances.url_livros, tecnologia.url_livros, terror.url_livros]
 
 cont = 0
 while cont < len(categorias):
---
+++ amazon-scraping/urls/economia.py
@@ -0,0 +1,14 @@
+categoria = 'Economia'
+
+url_livros = [
+    'https://www.amazon.com.br/Pai-Rico-Pobre-atualizada-ampliada-ebook/dp/B07H4WYN5M/ref=sr_1_1?rnid=5475882011&s=digital-text&sr=1-1',
+    'https://www.amazon.com.br/seis-li%C3%A7%C3%B5es-Ludwig-von-Mises-ebook/dp/B07BZVXGYL/ref=sr_1_2?rnid=5475882011&s=digital-text&sr=1-2',
+    'https://www.amazon.com.br/Riquezas-certas-para-voc%C3%AA-Douglas-ebook/dp/B08SSPR8LW/ref=sr_1_8?rnid=5475882011&s=digital-text&sr=1-8,'
+    'https://www.amazon.com.br/Tudo-precisa-saber-sobre-economia-ebook/dp/B0723GJ26R/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=economia&s=digital-text&sr=1-3',
+    'https://www.amazon.com.br/Economia-para-serve-como-funciona-ebook/dp/B078Z1F2ZG/ref=sr_1_4?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=economia&s=digital-text&sr=1-4',
+    'https://www.amazon.com.br/Ricos-s%C3%A1bios-felizes-investidores-bem-sucedido-ebook/dp/B099LPXJ4Y/ref=sr_1_11?rnid=5475882011&s=digital-text&sr=1-11',
+    'https://www.amazon.com.br/Economia-cotidiano-Decifra-me-Alexandre-Schwartsman-ebook/dp/B086667BQH/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=economia&s=digital-text&sr=1-6',
+    'https://www.amazon.com.br/%C3%9Altimos-escritos-econ%C3%B4micos-Karl-Marx-ebook/dp/B08HJLPGJK/ref=sr_1_16?rnid=5475882011&s=digital-text&sr=1-16',
+    'https://www.amazon.com.br/Brasil-uma-economia-que-aprende-ebook/dp/B089R462QF/ref=sr_1_18?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=economia&s=digital-text&sr=1-18',
+    'https://www.amazon.com.br/INTRODU%C3%87%C3%83O-%C3%80-ECONOMIA-PAULO-VICECONTI-ebook/dp/B076BZPH7Z/ref=sr_1_19?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=economia&s=digital-text&sr=1-19'
+]
