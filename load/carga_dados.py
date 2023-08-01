c67180e42c9609816e50441a3b220e75021f9493
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-08-01 20:43:37 -0300

    Refatoração do script de carga de dados

--- load/carga_dados.py
+++ load/carga_dados.py
@@ -1,14 +1,37 @@
-from Database import DataBase
-import pandas as pd
+import json
+from pymongo.mongo_client import MongoClient
 
-db = DataBase()
+uri = "mongodb://localhost:27017"
+client = MongoClient(uri)
+database = client['bookspace']
+collection = database['book']
 
-df = pd.read_csv('C:/Users/meerh/OneDrive/Documentos/SENAI/book-space-etl/transform/resultado/livros.csv')
+json_file = open('D:/Projects/Book Space/book-space-etl/transform/resultado/livros.json')
 
-tabelas = ['autor', 'idioma', 'genero', 'editora']
-for tabela in tabelas:
-    info = pd.DataFrame(df[tabela].unique())
-    info.columns = [tabela]
-    db.inserir_dados(info, tabela)
+data = json.load(json_file)
 
-db.inserir_livros(df)
+
+def inserir_livro(livro):
+    collection.insert_one(livro)
+
+
+cont = 0
+while cont < 89:
+    livro = {
+        'isbn_13': data['isbn_13'][f'{cont}'],
+        'isbn_10': data['isbn_10'][f'{cont}'],
+        'book_name': data['titulo'][f'{cont}'],
+        'pages': data['paginas'][f'{cont}'],
+        'description': data['descricao'][f'{cont}'],
+        'image_url': data['img_link'][f'{cont}'],
+        'authors': {
+            'author_1': data['autor'][f'{cont}']
+        },
+        'gender': {
+            'gender_1': data['genero'][f'{cont}']
+        },
+        'publisher': data['editora'][f'{cont}'],
+        'idiom': data['idioma'][f'{cont}']
+    }
+    inserir_livro(livro)
+    cont = cont + 1
